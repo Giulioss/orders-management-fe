941745875e25b1965dcf4082e10ac410729bd9cf
Giulio Galletti <giuliogallo22@gmail.com>
2024-10-17 22:28:15 +0200

    Aggiunta Angular Material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {jwtRequestInterceptor} from './common/interceptor/jwt.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -10,6 +11,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(
       withInterceptors([jwtRequestInterceptor])
-    ),
+    ), provideAnimationsAsync(),
   ]
 };
