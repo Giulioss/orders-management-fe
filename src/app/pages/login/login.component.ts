f0644acdf026429ef8d762fec0c9705ca488bd6d
Giulio Galletti <giuliogallo22@gmail.com>
2024-10-17 23:48:23 +0200

    Installazione bootstrap e miglioria grafica login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import {MatCard, MatCardContent, MatCardTitle} from '@angular/material/card';
+import {Component, OnInit} from '@angular/core';
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from '@angular/material/card';
 import {MatFormField} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -11,11 +12,24 @@ import {MatInput} from '@angular/material/input';
     MatCardTitle,
     MatFormField,
     MatInput,
-    MatCard
+    MatCard,
+    ReactiveFormsModule,
+    MatCardHeader
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup = new FormGroup([]);
+
+  constructor(private readonly fb: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    })
+  }
 
 }
