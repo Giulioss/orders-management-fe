784f0136fb45e37623e67dcbd6f2f70f7b8c63a2
Giulio Galletti <giuliogallo22@gmail.com>
2024-10-26 19:04:09 +0200

    Preaparazione tabella

--- src/app/component/orders-table/orders-table.component.ts
+++ src/app/component/orders-table/orders-table.component.ts
@@ -1,18 +1,50 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {OrderClient} from '../../common/clients/http-clients/order.client';
-import {OrderResponse} from '../../common/clients/responses/order.response';
 import {OrderFilterRequest} from '../../common/clients/requests/order-filter.request';
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef,
+  MatTable, MatTableDataSource, MatTextColumn
+} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort, MatSortHeader, MatSortModule} from '@angular/material/sort';
 
 @Component({
   selector: 'app-orders-table',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    MatHeaderCellDef,
+    MatCellDef,
+    MatHeaderRow,
+    MatHeaderRowDef,
+    MatRow,
+    MatRowDef,
+    MatTextColumn,
+    MatPaginator,
+    MatSortHeader,
+    MatSortModule
+  ],
   templateUrl: './orders-table.component.html',
   styleUrl: './orders-table.component.css'
 })
-export class OrdersTableComponent {
+export class OrdersTableComponent implements AfterViewInit {
 
-  ordersList: OrderResponse[] = [];
+  displayedColumns: string[] = ['id', 'note', 'orderStatus'];
+  dataSource = new MatTableDataSource();
+
+  @ViewChild(MatSort) sort: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
 
   constructor(private readonly orderClient: OrderClient) {
     this.getTableOrders();
@@ -25,7 +57,7 @@ export class OrdersTableComponent {
     }
 
     this.orderClient.getTableOrders(request).subscribe({
-      next: (orderList) => this.ordersList = orderList,
+      next: (orderList) => this.dataSource = new MatTableDataSource(orderList),
       error: (err) => console.error(err)
     });
   }
