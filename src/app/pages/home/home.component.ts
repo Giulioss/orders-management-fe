ccae3a5bb25c058734515dbafced5db78824b988
Giulio Galletti <giuliogallo22@gmail.com>
2024-10-26 17:35:18 +0200

    Creazione controller recupero lista ordini Preparazione componente interfaccia

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import {MatTab, MatTabGroup} from '@angular/material/tabs';
+import {OrdersTableComponent} from '../../component/orders-table/orders-table.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTabGroup,
+    MatTab,
+    OrdersTableComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
